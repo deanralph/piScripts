63fdfaa64c2e57f4d63fcf0ad5a44202902c87e0
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-01-31 11:00:31 +0000

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Shuts down the raspberry pi 
 
+if [ -d "/mnt/clusterShare/logs/shutdownLogs" ] 
+  then
+    mkdir /mnt/clusterShare/logs/shutdownLogs
+fi
+
 echo "Shutdown command recieved on $HOSTNAME at $date-%a" > /mnt/clusterShare/logs/shutdownLogs/$HOSTNAME+$date-%a
 
 shutdown -h now
