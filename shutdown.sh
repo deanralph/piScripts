ac7c4e2ce5a6214ababd74b6b6eec08f95e5c948
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-01-31 12:33:45 +0000

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Shuts down the raspberry pi & loggs the time
 
-if [ -d "/mnt/clusterShare/logs/shutdownLogs\$HOSTNAME" ] 
+if [ -d "/mnt/clusterShare/logs/shutdownLog/$HOSTNAME" ] 
   then
     echo "Log folder not found - creating it"
-    mkdir /mnt/clusterShare/logs/shutdownLogs\$SOSTNAME
+    mkdir /mnt/clusterShare/logs/shutdownLogs/$SOSTNAME
 fi
 
-echo "Shutdown command recieved on $HOSTNAME at $date-%a" > /mnt/clusterShare/logs/shutdownLogs/$HOSTNAME+$date-%a
+echo "Shutdown command recieved on $HOSTNAME at $(date-%a)" > /mnt/clusterShare/logs/shutdownLogs/$HOSTNAME/$(date +'%Y%m%d%H%M')
 
 shutdown -h now
