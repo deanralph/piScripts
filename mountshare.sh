90b8037c45be42e08c4e72aa5fbd7187cec8d81d
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-01-31 12:06:37 +0000

    Update mountshare.sh

--- mountshare.sh
+++ mountshare.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # mounts shared area
 
-sudo mount.cifs //arther/clusterShare /mnt/clusterShare -o user=guest
+if [ ! -d /mnt/clusterShare ]
+then
+  sudo mount.cifs //arther/clusterShare /mnt/clusterShare -o user=guest 
+fi
