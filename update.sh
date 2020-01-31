8c88d5b5157631f33601eebb53b5fed4b698cec9
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-01-31 10:40:24 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,16 @@ mkdir logs/
 
 updatelog=logs/$(date +'%Y%m%d%H%M')
 
-apt-get update >> $updatelog
+echo "Checking For Updates..."
+apt-get update > $updatelog
+
+echo "Applying Updates..."
 apt-get upgrade -y >> $updatelog
+
+echo "Removing defunct packages..."
 apt-get autoremove >> $updatelog
+
+echo "Cleaning up..."
 apt-get autoclean >> $updatelog
+
+echo "Done!"
