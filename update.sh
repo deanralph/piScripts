b8f8d726d6b667bf0c1582abbe16b821f6863d9d
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-01-31 10:12:07 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # Runs update routine
 
+backup_dir=$(date +'%m/%d/%Y')
+echo "Backup dir for today: /logs/${backup_dir}"
+
+mkdir /logs/$backup_dir/
+
+mv updatelog /logs/$backup_dir/updatelog
+
 apt-get update apt-get upgrade -y && apt-get autoremove && apt-get autoclean >> updatelog
